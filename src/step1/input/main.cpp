74c76405e4d668650d8a2469c79cc5e6fd5ac740
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-02-28 12:10:16 -0600

    example
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- src/step1/input/main.cpp
+++ src/step1/input/main.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    float monica_buffer2 =20.37;
-    for (int i=0; i<=10; i++){
+    float monica_buffer2=20.37;
+    for(int i=0;i<=10; i++){
         printf("value: %d", monica_buffer2);
     }
+    int hola = 23,
     return 0;
 }
